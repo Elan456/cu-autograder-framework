75bc9c5c45af3d56d908b9ccab0ed18ea7aa9bfb
Ethan Anderson <telan4892@gmail.com>
2023-12-11 16:21:49 -0500

    initial commit

---
+++ tests/test.py
@@ -0,0 +1,32 @@
+"""
+This is the primary testing file which will hold all of the test cases 
+that can contribute to the student's score. 
+"""
+
+import unittest  # Python's unit testing library
+import os  #  For moving files looking inside of folders
+import subprocess  #  For running executables and saving their stdout
+
+# All of Gradescope's special decorators to modify how the tests are weighted and displayed
+from gradescope_utils.autograder_utils.decorators import visibility, weight, number, leaderboard
+
+
+class test01_setup(unittest.TestCase):
+
+    # Files that must be in their submission
+    # They will fail the first test case if they don't have these files
+    required_files = []
+
+    # Files that can be in their submission and should be 
+    # copied over. These could be extra credit files
+    optional_files = []
+
+
+    @number("0") # Does not affect execution order 
+    @weight(0)
+    def test_checkFiles(self):
+        """Expected files are present"""
+
+        # Checking if the required files all exist
+
+
