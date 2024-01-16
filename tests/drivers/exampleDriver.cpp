39d5d665252d9617dc80b967f50b19a6612c7344
Elan456 <telan4892@gmail.com>
2024-01-15 22:52:12 -0500

    Fixes from making an example

--- tests/drivers/exampleDriver.cpp
+++ tests/drivers/exampleDriver.cpp
@@ -1,37 +1,36 @@
 #include "studentCode.h"
-#include <stdio.h>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
 // Outputs if the case failed or passed
 // e.g.
 // Case myTest pass
-void caseOutput(char* name, bool pass){
-  printf("Case %s ", name);
-  if (pass){
-    printf("pass\n");
+void caseOutput(string name, bool pass) {
+  cout << "Case " << name << " "; 
+  if (pass) {
+	  cout << "pass" << endl;
   } else {
-    printf("fail\n");
+	  cout << "fail" << endl;
   }
 }
 
-
-// Don't put too many tests in one file 
+// Don't put too many tests in one file
 // If one causes a seg fault or infinite loop, then the rest of the tests won't
 // be run
-int main (){
+int main() {
   // Test 1
-  caseOutput("3+3=6", studentAdd(3,3)=6);
+  caseOutput("3+3=6", studentAdd(3, 3) == 6);
 
   // Test 2
-  caseOutput("1+8=9", studentAdd(1,8)=9);
+  caseOutput("1+8=9", studentAdd(1, 8) == 9);
 
   // Test 3
-  caseOutput("100+500=600", studentAdd(100,500)=600);
+  caseOutput("100+500=600", studentAdd(100, 500) == 600);
 
-  // Test 4 
-  caseOutput("1+1=2", studentAdd(1,1)=2);
-
+  // Test 4
+  caseOutput("1+1=2", studentAdd(1, 1) == 2);
 
   // Outputting a note that the all the test finished
   // If this case isn't detected in stdout, then either a
