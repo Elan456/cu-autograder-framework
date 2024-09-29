8e2b16c92e17f209e31a8312efc9ef184bd20c6f
anabasis <anabasis@noreply.codeberg.org>
2024-09-29 11:18:41 -0400

    fix: exit if unable to download harness

--- setup.sh
+++ setup.sh
@@ -25,10 +25,14 @@ chmod go=rwx /autograder/source  # So they can create files when compiling
 
 # create temp file to download harness to
 TMP_HARNESS="$(mktemp)"
-curl 'https://s3-us-west-2.amazonaws.com/gradescope-static-assets/autograder/python3/harness.py' -o "$TMP_HARNESS"
+curl -f 'https://s3-us-west-2.amazonaws.com/gradescope-static-assets/autograder/python3/harness.py' -o "$TMP_HARNESS" || 
+	(echo 'Unable to download harness for additional security checks! Please check the harness URL!' && exit 0)
 
 # find line number of run_autograder function
-line_num="$(grep -n 'def run_autograder' "$TMP_HARNESS" | cut -f1 -d:)"
+line_num="$(grep -m1 -n 'def run_autograder' "$TMP_HARNESS" | cut -f1 -d:)"
+
+[ -z "$line_num" ] && echo 'Unable to find function to add security checks!' && exit 0
+
 # store checksum for run_autograder script
 checksum="$(sha256sum /autograder/run_autograder)"
 
